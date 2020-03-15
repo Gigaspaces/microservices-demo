fc0d6beff8632cc6cf461a277e3e2c68964a9352
yaelna <nahon.yael@gmail.com>
2020-03-15 15:33:45 +0200

    deploy demo script

---
+++ bin/start-demo.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -ex
+
+echo "Downloading gsctl.jar"
+
+wget https://github.com/Gigaspaces/gsctl/releases/download/15.2.0-m15/gsctl.jar
+
+
+echo "Setting up cluster"
+java -jar gsctl.jar create
+
+echo "Deploying services"
+for space in kitchen-space delivery-space orders-space; do
+	java -jar gsctl.jar deploy STATEFUL $space https://github.com/Gigaspaces/microservices-demo/raw/master/jars/$space.jar
+done
+
+for service in kitchen-service delivery-service orders-service gateway-api user-app; do
+	java -jar gsctl.jar deploy WEB $service https://github.com/Gigaspaces/microservices-demo/raw/master/jars/$service.war
+done
+
+java -jar gsctl.jar list-services
