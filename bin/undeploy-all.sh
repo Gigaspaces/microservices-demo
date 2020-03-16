c5917f6c493467979e3c32b75558cbb34604fb0a
yaelna <nahon.yael@gmail.com>
2020-03-16 21:15:52 +0200

    fixed preloader

--- bin/undeploy-all.sh
+++ bin/undeploy-all.sh
@@ -3,5 +3,5 @@
 set -ex
 
 for space in kitchen-space delivery-space orders-space kitchen-service delivery-service orders-service gateway-api user-app; do
-	./gsctl undeploy $space
+	java -jar gsctl.jar undeploy $space
 done
--- kitchen-service/src/main/java/com/gigaspaces/ndemo/KitchenPreloader.java
+++ kitchen-service/src/main/java/com/gigaspaces/ndemo/KitchenPreloader.java
@@ -28,54 +28,63 @@ public class KitchenPreloader {
         gigaSpace.write(new MenuItem("Spicy tuna roll", restaurant.getId()));
 
         restaurant = new Restaurant("Five Guys", "Down town");
+        gigaSpace.write(restaurant);
         gigaSpace.write(new MenuItem("Hamburger", restaurant.getId()));
         gigaSpace.write(new MenuItem("Cheeseburger", restaurant.getId()));
         gigaSpace.write(new MenuItem("Franch fries", restaurant.getId()));
         gigaSpace.write(new MenuItem("Onion rings", restaurant.getId()));
 
         restaurant = new Restaurant("Burgus", "Down town");
+        gigaSpace.write(restaurant);
         gigaSpace.write(new MenuItem("Hamburger", restaurant.getId()));
         gigaSpace.write(new MenuItem("Cheeseburger", restaurant.getId()));
         gigaSpace.write(new MenuItem("Franch fries", restaurant.getId()));
         gigaSpace.write(new MenuItem("Onion rings", restaurant.getId()));
 
         restaurant = new Restaurant("Tony's Pizza", "West side");
+        gigaSpace.write(restaurant);
         gigaSpace.write(new MenuItem("Small pizza", restaurant.getId()));
         gigaSpace.write(new MenuItem("Large pizza", restaurant.getId()));
         gigaSpace.write(new MenuItem("Garlic bread", restaurant.getId()));
         gigaSpace.write(new MenuItem("Soda", restaurant.getId()));
 
         restaurant = new Restaurant("Moon Sushi", "West side");
+        gigaSpace.write(restaurant);
         gigaSpace.write(new MenuItem("Salmon sashimi", restaurant.getId()));
         gigaSpace.write(new MenuItem("Tuna sashimi", restaurant.getId()));
         gigaSpace.write(new MenuItem("Philadelphia roll", restaurant.getId()));
         gigaSpace.write(new MenuItem("Spicy tuna roll", restaurant.getId()));
 
         restaurant = new Restaurant("Frank's", "West side");
+        gigaSpace.write(restaurant);
         gigaSpace.write(new MenuItem("Hotdog", restaurant.getId()));
         gigaSpace.write(new MenuItem("Chilly hotdog", restaurant.getId()));
         gigaSpace.write(new MenuItem("Fries", restaurant.getId()));
         gigaSpace.write(new MenuItem("Beer", restaurant.getId()));
 
         restaurant = new Restaurant("Falafel Corner", "Up town");
+        gigaSpace.write(restaurant);
         gigaSpace.write(new MenuItem("Lamb moroccan style", restaurant.getId()));
         gigaSpace.write(new MenuItem("Spicy beef", restaurant.getId()));
         gigaSpace.write(new MenuItem("Chicken Moroccan style", restaurant.getId()));
         gigaSpace.write(new MenuItem("Shawarma", restaurant.getId()));
 
         restaurant = new Restaurant("Pizza Domino's", "Up town");
+        gigaSpace.write(restaurant);
         gigaSpace.write(new MenuItem("Small pizza", restaurant.getId()));
         gigaSpace.write(new MenuItem("Large pizza", restaurant.getId()));
         gigaSpace.write(new MenuItem("Garlic bread", restaurant.getId()));
         gigaSpace.write(new MenuItem("Soda", restaurant.getId()));
 
         restaurant = new Restaurant("Curry Kitchen", "Up town");
+        gigaSpace.write(restaurant);
         gigaSpace.write(new MenuItem("Butter chicken", restaurant.getId()));
         gigaSpace.write(new MenuItem("Tandoori chicken", restaurant.getId()));
         gigaSpace.write(new MenuItem("Chicken tikka masala", restaurant.getId()));
         gigaSpace.write(new MenuItem("Malai kofta", restaurant.getId()));
 
         restaurant = new Restaurant("Spoons Cafe", "Up town");
+        gigaSpace.write(restaurant);
         gigaSpace.write(new MenuItem("Omelette sandwich", restaurant.getId()));
         gigaSpace.write(new MenuItem("Tuna sandwich", restaurant.getId()));
         gigaSpace.write(new MenuItem("Bruschetta", restaurant.getId()));
