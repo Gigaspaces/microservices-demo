53cb54fb83131fe5a38bd90cd77d780c6ce0f9e4
yohanakh <yohana.khoury@gmail.com>
2020-06-15 14:39:32 +0300

    Updated demo to use m10 with TLS enabled

--- bin/start-demo.sh
+++ bin/start-demo.sh
@@ -4,18 +4,18 @@ set -ex
 
 echo "Downloading gsctl.jar"
 
-wget https://gigaspaces-releases-eu.s3.amazonaws.com/gsctl/15.2.0/gsctl.jar
+wget https://gigaspaces-releases-eu.s3.amazonaws.com/gsctl/15.5.0-m10/gsctl.jar
 
 echo "Setting up cluster"
 java -jar gsctl.jar create
 
 echo "Deploying services"
 for space in kitchen-space delivery-space orders-space; do
-	java -jar gsctl.jar deploy STATEFUL $space https://github.com/Gigaspaces/microservices-demo/raw/master/jars/$space.jar
+	java -jar gsctl.jar deploy $space https://github.com/Gigaspaces/microservices-demo/raw/master/jars/$space.jar
 done
 
 for service in kitchen-service delivery-service orders-service gateway-api user-app; do
-	java -jar gsctl.jar deploy WEB $service https://github.com/Gigaspaces/microservices-demo/raw/master/jars/$service.war
+	java -jar gsctl.jar deploy $service https://github.com/Gigaspaces/microservices-demo/raw/master/jars/$service.war
 done
 
 java -jar gsctl.jar list-services
