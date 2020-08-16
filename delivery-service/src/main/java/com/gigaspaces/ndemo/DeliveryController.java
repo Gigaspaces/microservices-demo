39a5bd5a0f648d96b4ede2a9be25477f71989c28
yaelna <nahon.yael@gmail.com>
2020-08-16 12:59:39 +0300

    support delivery-service restarts

--- delivery-service/src/main/java/com/gigaspaces/ndemo/DeliveryController.java
+++ delivery-service/src/main/java/com/gigaspaces/ndemo/DeliveryController.java
@@ -6,6 +6,11 @@ import com.gigaspaces.order.model.DeliverOrderRequest;
 import com.gigaspaces.order.model.OrderStatusMsg;
 import com.gigaspaces.order.model.Status;
 import com.gigaspaces.order.model.UpdateOrderRequest;
+import com.gigaspaces.query.aggregators.AggregationResult;
+import com.gigaspaces.query.aggregators.AggregationSet;
+import com.gigaspaces.query.aggregators.SpaceEntriesAggregator;
+import com.gigaspaces.query.aggregators.SpaceEntriesAggregatorContext;
+import com.j_spaces.core.client.SQLQuery;
 import io.opentracing.Span;
 import io.opentracing.Tracer;
 import io.opentracing.util.GlobalTracer;
@@ -23,29 +28,23 @@ import java.util.logging.Logger;
 @RestController
 public class DeliveryController {
 
+    private static AtomicLong idGenerator = new AtomicLong(System.currentTimeMillis());
+    private static Logger logger = Logger.getLogger("DEBUG_YAEL_LOGGER");
     @Autowired
     private GigaSpace gigaSpace;
-
     @Autowired
     private ServicesDiscovery servicesDiscovery;
-
     @Autowired
     private RestTemplate restTemplate;
-
     @Autowired
     private TracingSpanMap tracingSpanMap;
-
-    private static AtomicLong idGenerator = new AtomicLong(System.currentTimeMillis());
-
-    private static Logger logger = Logger.getLogger("DEBUG_YAEL_LOGGER");
 
     @PostMapping("/deliver")
     public void deliverOrder(@RequestBody DeliverOrderRequest deliverOrderRequest) throws Exception {
         wrap("delivery-service : delivery", () -> {
-
-            logger.severe("%%%%%%%%% deliver request order id = "+deliverOrderRequest.getOrderId()+" %%%%%%%%%");
+            logger.info("%%%%%%%%% deliver request order id = " + deliverOrderRequest.getOrderId() + " %%%%%%%%%");
             Delivery delivery = new Delivery();
-            delivery.setDeliveryId(String.valueOf(idGenerator.incrementAndGet()));
+            delivery.setDeliveryId(String.valueOf(getGenerator().incrementAndGet()));
             delivery.setOrderId(deliverOrderRequest.getOrderId());
             delivery.setRegion(deliverOrderRequest.getRegion());
             delivery.setTaken(false);
@@ -62,6 +61,21 @@ public class DeliveryController {
 
             return null;
         });
+    }
+
+    private AtomicLong getGenerator() {
+        if (idGenerator == null) {
+            if (gigaSpace.count(new Delivery()) == 0) {
+                logger.info("initializing idGenerator to 0");
+                idGenerator = new AtomicLong(System.currentTimeMillis());
+            } else {
+                AggregationResult result = gigaSpace.aggregate(new SQLQuery<Delivery>(), new AggregationSet().add(new MaxDeliveryIdAggregator()));
+                long initialValue = ((Long) result.get("max(deliveryId)")) + 1;
+                logger.info("initializing idGenerator to "+initialValue);
+                idGenerator = new AtomicLong(initialValue);
+            }
+        }
+        return idGenerator;
     }
 
 
@@ -90,4 +104,32 @@ public class DeliveryController {
     }
 
 
+    private class MaxDeliveryIdAggregator extends SpaceEntriesAggregator<Long> {
+        private transient Long result = null;
+
+        @Override
+        public String getDefaultAlias() {
+            return "max(deliveryId)";
+        }
+
+        @Override
+        public void aggregate(SpaceEntriesAggregatorContext context) {
+            String deliveryId = (String) context.getPathValue("deliveryId");
+            if (deliveryId != null) {
+                long value = Long.parseLong(deliveryId);
+                result = result == null || result < value ? value : result;
+            }
+        }
+
+        @Override
+        public Long getIntermediateResult() {
+            return result;
+        }
+
+
+        @Override
+        public void aggregateIntermediateResult(Long partitionResult) {
+            result = result == null || result < (long) partitionResult ? (long) partitionResult : result;
+        }
+    }
 }
